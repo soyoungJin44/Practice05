9a73505b39777d4086d43ea8b2af8c2a50b8bc01
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-03 16:33:35 +0900

    1문 완

--- src/com/javaex/ex01/Member.java
+++ src/com/javaex/ex01/Member.java
@@ -1,5 +1,47 @@
 package com.javaex.ex01;
 
 public class Member {
+	
+	//필드
+	
+	private String id;
+	private String name;
+	private int point;
+	
 
+	//생성자
+	public Member() {
+		
+	}
+	public Member(String id, String name, int point) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.point = point;
+	}
+	
+	//메서드 gs
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+	//메서드 일반
+	public void showInfo() {
+		System.out.println("회원정보: " + name + "(" + id + "), " + point + "점"); 
+	}
 }
--- src/com/javaex/ex01/MemberApp.java
+++ src/com/javaex/ex01/MemberApp.java
@@ -3,8 +3,14 @@ package com.javaex.ex01;
 public class MemberApp {
 
 	public static void main(String[] args) {
-
 		
+		Member member1 = new Member("정우성","jws",50000);
+		Member member2 = new Member("유재석","yjs",30000);
+		Member member3 = new Member("이효리","lhr",40000);
+		
+		member1.showInfo();
+		member2.showInfo();
+		member3.showInfo();
 	}
 
 }
