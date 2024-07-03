b4c55764c8c304fd58a25bc45040182fb8f6d50d
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-03 16:51:02 +0900

    3문 완

--- src/com/javaex/ex03/Song.java
+++ src/com/javaex/ex03/Song.java
@@ -1,6 +1,34 @@
 package com.javaex.ex03;
 
 public class Song {
+	//배열
+	private String title;
+	private String artist;
+	private String album;
+	private String computer;
+	private String year;
+	private int track;
+	//생성자
+	public Song() {
+		System.out.println("Song(파라미터0개)");
+	}
+	//메서드 gs
+	
+	//메서드 일반
+	public Song(String title, String artist, String album, String computer, String year) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.computer = computer;
+		this.year = year;
+		System.out.println("Song(파라미터5개)");
+	}
+	
+	public Song(String title, String artist, String album, String computer, String year, int track) {
+		this(title,artist,album,computer,year);
+		this.track = track;
+		System.out.println("Song(파라미터6개)");
+	}
 	
 	
 	
