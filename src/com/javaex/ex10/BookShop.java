452d251c87f4d197eabaa52f4ba6f9831538bdec
찐 <@DESKTOP-LMO9JQ3>
2024-07-06 12:16:13 +0900

    .

--- src/com/javaex/ex10/BookShop.java
+++ src/com/javaex/ex10/BookShop.java
@@ -28,7 +28,7 @@ public class BookShop {
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
         // 코드작성
-        
+        books[num-1].rent();
         
 
         System.out.println("*****도서 정보 출력하기******");
